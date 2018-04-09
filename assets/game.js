33e7b182feb5f76652a7155faefb9e0f806fcee7
Kacie Nguyen <maidiep.nguyen@hotmail.com>
2018-04-09 11:24:37 -0400

    able to test setTimeOut function to work.

--- assets/game.js
+++ assets/game.js
@@ -23,7 +23,7 @@ function countdown(minutes) {
     tick();
 }
 
-countdown(3);
+countdown(1);
 
 // //which radio button was selected?
 //     $( "input[type='submit']" ).click(function( event ) {
@@ -32,4 +32,46 @@ countdown(3);
 //       }
 //       event.preventDefault();
 //   });
+  
+
+
+//function to check answers
+    function checkAnswers() {
+        var q1=document.trivia.q1.value;
+        var q2=document.trivia.q2.value;
+        var q3=document.trivia.q3.value;
+        var q4=document.trivia.q4.value;
+        var q5=document.trivia.q5.value;
+        var correct = 0;
+        var incorrect = 0;
+
+        if(q1=="b") {
+            correct++;
+        }  else {
+            incorrect++;
+        }
+        if(q2=="c") {
+            correct++
+        }  else {
+            incorrect++;
+        } 
+       if(q3=="b") {
+            correct++;
+        }  else {
+            incorrect++;
+        }
+       if (q4=="b") {
+           correct++;
+        }  else {
+            incorrect++;
+        }
+       if (q5=="c") {
+           correct++;
+        }  else {
+            incorrect++;
+        }
+       alert("You got " + correct + " correct. and " + incorrect + " incorrect");
+    }
+
+   setTimeout (checkAnswers, 5000);
   
