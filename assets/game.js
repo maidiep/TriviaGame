cdcb22c0443df9b308299d0d665233ae53c01995
Kacie Nguyen <maidiep.nguyen@hotmail.com>
2018-04-08 22:22:19 -0400

    fixed timer code.

--- assets/game.js
+++ assets/game.js
@@ -1,11 +1,10 @@
-//timer
 var timeoutHandle;
 function countdown(minutes) {
-    var seconds = 30;
-    var mins = minutes
+    var seconds = 60;
+    var mins = minutes;
     function tick() {
-        var counter = document.getElementById("seconds");
-        var current_minutes = 0
+        var counter = document.getElementById("timer");
+        var current_minutes = mins-1
         seconds--;
         counter.innerHTML =
         current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
@@ -24,8 +23,7 @@ function countdown(minutes) {
     tick();
 }
 
-countdown();
-// count();
+countdown(3);
 
 //which radio button was selected?
     $( "input[type='submit']" ).click(function( event ) {
